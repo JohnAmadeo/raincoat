c1064713e4bd1fd3b71c310665b2b2449623e704
Shea Ketsdever <sheaketsdever@Sheas-MacBook-Pro.local>
2017-11-23 16:38:13 -0800

    Webscraping now iterates over all of IMDb top 250.

--- webscraper.py
+++ webscraper.py
@@ -1,55 +1,114 @@
 from bs4 import BeautifulSoup
 import urllib
 import re
+import requests
 
-# INITIALIZE BEAUTIFUL SOUP
+def get_urls():
+	r1 = urllib.urlopen('http://www.imdb.com/chart/top').read()
+	soup1 = BeautifulSoup(r1, 'html.parser')
+	
+	movie_entries = soup1.findAll("td", "titleColumn")
+	urls = []
+	for movie in movie_entries:
+		href = movie.find("a")["href"]
+		url = "http://www.imdb.com" + href
+		urls.append(url)
 
-r = urllib.urlopen('http://www.imdb.com/title/tt0120338/?ref_=nv_sr_1').read()
-soup = BeautifulSoup(r, 'html.parser')
+	return urls
 
-# EVALUATION METRICS: METACRITIC & IMDB SCORES, GROSS PROFIT
+def parse_url(url):
 
-metacritic_score = int(soup.find('div', 'metacriticScore').span.text)
-print metacritic_score
+	# INITIALIZE BEAUTIFUL SOUP
 
-imdb_score = float(soup.find('span', 'rating-rating ').find('span', 'value').text)
-print imdb_score
+	r = urllib.urlopen(url).read()
+	soup = BeautifulSoup(r, 'html.parser')
 
-gross = str(soup.find("h4", text=re.compile("Gross:")).next_sibling)
-gross = gross.replace(' ', '')	# remove extra spaces at beginning
-gross = gross.replace('$', '')	# remove dollar sign
-gross = gross.replace('\n', '')	# remove extra newlines
-gross = gross.replace(',', '')	# remove commas so it can be converted to an int
-gross = int(gross)
-print gross
+	# EVALUATION METRICS: METACRITIC & IMDB SCORES, GROSS PROFIT
 
-# MOVIE DETAILS
+	metacritic_score = soup.find('div', 'metacriticScore')
+	if metacritic_score:	# for the rare case where a movie doesnt have a metacritic score (eg. "city lights" by charles chaplin)
+		metacritic_score = int(metacritic_score.span.text)
 
-title = str(soup.find('title').text)
-print title
+	imdb_score = soup.find('span', 'rating-rating ')
+	if imdb_score:
+		imdb_score = float(imdb_score.find('span', 'value').text)
 
-budget = str(soup.find("h4", text=re.compile("Budget:")).next_sibling)
-budget = budget.replace(' ', '')	# remove extra spaces at beginning
-budget = budget.replace('$', '')	# remove dollar sign
-budget = budget.replace('\n', '')	# remove extra newlines
-budget = budget.replace(',', '')	# remove commas so it can be converted to an int
-budget = int(budget)
-print budget
+	gross = soup.find("h4", text=re.compile("Gross:"))
+	if gross:
+		gross = u''.join(gross.next_sibling).encode('utf-8')
+		gross = gross.replace(' ', '')	# remove extra spaces at beginning
+		gross = gross.replace('$', '')	# remove dollar sign
+		gross = gross.replace('\n', '')	# remove extra newlines
+		gross = gross.replace(',', '')	# remove commas so it can be converted to an int
+		gross = int(gross)
 
-director = str(soup.find("h4", text=re.compile("Director:")).next_sibling.next_sibling.get_text())
-director = director.replace('\n', '')
-print director
+	# MOVIE DETAILS
 
-writer = str(soup.find("h4", text=re.compile("Writer:")).next_sibling.next_sibling.get_text())
-writer = writer.replace('\n', '')
-print writer
+	title = u''.join(soup.find('title').text).encode('utf-8')
+	title = title[:title.index("(")]	# get rid of the "(date) - IMDb"
 
-genre = str(soup.find("h4", text=re.compile("Genres:")).next_sibling.next_sibling.get_text())[1:]
-print genre
+	budget = soup.find("h4", text=re.compile("Budget:"))
+	if budget:	# there are a few movies that don't have budgets listed. for instance , "M".
+		budget = budget.next_sibling
+		budget = ''.join(x for x in budget if x.isdecimal())	# remove extra spaces, dollar sign, newlines, commas, etc.
+		budget = int(budget)
 
-cast = soup.find("table", "cast_list").findAll("span", itemprop="name")
-top_five_actors = []
-for x in xrange(5):
-	actor = str(cast[x].text)
-	top_five_actors.append(actor)
-print top_five_actors
+	director = soup.find("h4", text=re.compile("Director:"))
+	if director:
+		director = u''.join(director.next_sibling.next_sibling.get_text()).encode('utf-8')
+	else:
+		director = u''.join(soup.find("h4", text=re.compile("Directors:")).next_sibling.next_sibling.get_text()).encode('utf-8')
+	director = director.replace('\n', '')
+	try:
+		director = director[:director.index("(")]	# get rid of extra info like "(screenplay)"
+	except ValueError:
+		director = director
+
+	writer = soup.find("h4", text=re.compile("Writer:"))
+	if writer:
+		writer = u''.join(writer.next_sibling.next_sibling.get_text()).encode('utf-8')
+	else:
+		writer = u''.join(soup.find("h4", text=re.compile("Writers:")).next_sibling.next_sibling.get_text()).encode('utf-8')
+	writer = writer.replace('\n', '')
+	writer = writer.replace(',', '')	# if multiple writers are listed the comma will be attached to the first name; delete it
+	try:
+		writer = writer[:writer.index("(")]	# get rid of extra info like "(screenplay)"
+	except ValueError:
+		writer = writer
+
+	genre = u''.join(soup.find("h4", text=re.compile("Genres:")).next_sibling.next_sibling.get_text()).encode('utf-8')[1:]
+
+	cast = soup.find("table", "cast_list").findAll("span", itemprop="name")
+	top_five_actors = []
+	for x in xrange(min(5, len(cast))):
+		actor = u''.join(cast[x].text).encode('utf-8')
+		top_five_actors.append(actor)
+
+	# PRINT RESULTS
+
+	print "//////////////////////////////////////////////////////////////////////"
+	if title:
+		print "TITLE: " + title
+	if metacritic_score:
+		print "METACRITIC SCORE: " + str(metacritic_score)
+	if imdb_score:
+		print "IMDB SCORE: " + str(imdb_score)
+	if gross:
+		print "GROSS: " + str(gross)
+	if budget:
+		print "BUDGET: " + str(budget)
+	if director:
+		print "DIRECTOR: " + director
+	if writer:
+		print "WRITER: " + writer
+	if genre:
+		print "GENRE: " + genre
+	if top_five_actors:
+		print "TOP 5 ACTORS: "
+		print top_five_actors
+
+
+# MAIN CALL
+urls = get_urls()
+for url in urls:
+	parse_url(url)
